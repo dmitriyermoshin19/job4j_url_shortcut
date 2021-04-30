7af7cd5af1210ffd9eace46aed690f22645c5c68
dmitriyermoshin19 <dmstrannik@yandex.ru>
2021-04-30 10:51:31 +0300

    2. Сервис - UrlShortCut[13799#378368]

---
+++ src/main/java/ru/job4j/shortcut/domains/Views.java
@@ -0,0 +1,15 @@
+package ru.job4j.shortcut.domains;
+
+public class Views {
+    public interface Converter {
+    }
+
+    public interface Redirecter {
+    }
+
+    public interface Statistic extends Converter {
+    }
+
+    public interface User {
+    }
+}
